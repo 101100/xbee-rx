77975077dd801ea32ceec774d3d5c64761f5e633
Jason Heard <jasonpheard@gmail.com>
2014-05-04 10:46:36 -0600

    Fix blink.js example

--- examples/blink.js
+++ examples/blink.js
@@ -1,22 +1,51 @@
-var xbee = require('../lib/xbee-promise.js');
+/*jslint node:true */
+
+/*
+ * blink.js
+ * https://github.com/101100/xbee-promise
+ *
+ * Example that blinks a LED on AD1 on a remote module
+ *
+ * Copyright (c) 2014 Jason Heard
+ * Licensed under the MIT license.
+ */
+
+"use strict";
+
+var xbeePromise = require('../lib/xbee-promise.js');
+
+var xbee = xbeePromise({
+    serialport: '/dev/ttyUSB0',
+    serialportOptions: {
+        baudrate: 57600
+    },
+    // turn on debugging to see what the library is doing
+    debug: false
+});
 
 var onNext = true;
-var nodeId = 'TEMP3';
+var nodeId = "TEMP3";
 
 console.log('Blinking LED on AD1 on module with ID: ', nodeId);
 
-var interval = setInterval(function () {
+function toggleLed() {
+    process.stdout.write("Turning LED " + (onNext ? "ON." : "OFF") + "...");
+
     xbee.remoteCommand({
         command: "D1",
         commandParameter: [ onNext ? 5 : 4 ],
         destinationId: nodeId
-    }).catch(function(e) {
-        console.log("Command failed:", e);
-        clearInterval(interval);
+    }).then(function toggleSuccess() {
+        console.log(" success.");
+        onNext = !onNext;
+
+        // schedule next toggle in 2 seconds
+        setTimeout(toggleLed, 2000);
+    }).catch(function toggleFailure(e) {
+        console.log(" command failed:\n", e);
         xbee.close();
     });
-    console.log(onNext ? 'ON' : 'OFF');
+}
 
-    onNext = !onNext;
-}, 2000);
-
+// do initial toggle
+setImmediate(toggleLed);
